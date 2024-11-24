b5d4c3498e426c106e51d06f41b87301636f2d1a
Shahriar-Sahaf <sahafshahriar@gmail.com>
2024-11-24 14:31:59 +0330

    check sahaf

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import secrets
 class Config:
     SECRET_KEY = secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgresql://username:password@localhost/dbname'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
+    #Check
